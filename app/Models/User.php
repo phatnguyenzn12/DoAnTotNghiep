bc62707dbd0d9ab56cb9ae354658ee6ca52ef70c
Phát Nguyễn <phatnguyenzn12@gmail.com>
2022-11-12 15:26:23 +0700

    update login

--- app/Models/User.php
+++ app/Models/User.php
@@ -93,7 +93,7 @@ class User extends Authenticatable
         $res = DB::table($this->table)->where('id', $id)->update(['password'=>$password,'remember_token'=>null]);
         return $res;
     }
-
+    
     public function loadOne($id)
     {
         $query = DB::table($this->table)->where('id', '=', $id);
