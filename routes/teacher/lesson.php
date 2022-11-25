d54117b9fed660962e9a3bfe9bc6207bdedc5f90
thuy1002 <thuy1002dangthanh@gmail.com>
2022-11-25 17:06:08 +0700

    update code

---
+++ routes/teacher/lesson.php
@@ -0,0 +1,16 @@
+<?php
+
+
+use App\Http\Controllers\mentor\CourseController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('mentor/course')->name('mentor.course.')->controller(CourseController::class)->group(
+    function () {
+        Route::get('index', 'index')->name('index');
+        Route::get('create', 'create')->name('create');
+        Route::post('store', 'store')->name('store');
+        Route::get('edit-program/{course_id}', 'program')->name('program');
+        Route::get('edit-course/{id}', 'edit')->name('edit');
+        Route::put('update-course/{course}', 'update')->name('update');
+    }
+);
