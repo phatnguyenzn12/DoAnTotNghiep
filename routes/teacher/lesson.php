a7e91aca5fc605409fa96736f9d5f3a8f260cac5
thuy1002 <thuy1002dangthanh@gmail.com>
2022-11-26 23:46:17 +0700

    update code

--- routes/teacher/lesson.php
+++ routes/teacher/lesson.php
@@ -8,8 +8,8 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('teacher/lesson')->name('teacher.lesson.')->middleware(['role:mentor','roles:teacher'])->controller(LessonController::class)->group(
     function () {
         Route::get('list/{id}', 'list')->name('list');
-        Route::get('add/{chapter}', 'create')->name('add');
-        Route::get('add', 'create')->name('add');
+        Route::get('add/{chapter_id}', 'create')->name('add');
+     //   Route::get('add', 'create')->name('add');
         Route::post('store', 'store')->name('store');
         Route::get('edit-program/{course_id}', 'program')->name('program');
         Route::get('edit-course/{id}', 'edit')->name('edit');
